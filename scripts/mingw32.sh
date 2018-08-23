7589bca3e2b6551c442be51a38560e208378f71a
Marc Hoersken <info@marc-hoersken.de>
2018-08-23 21:24:17 +0200

    Initial commit

---
+++ scripts/mingw32.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PATH="/mingw32/bin:$PATH"
+export C_INCLUDE_PATH="/mingw32/include:/mingw32/i686-w64-mingw32/include"
+export LD_LIBRARY_PATH="/mingw32/lib:/mingw32/i686-w64-mingw32/lib"
---
+++ scripts/mingw64.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PATH="/mingw64/bin:$PATH"
+export C_INCLUDE_PATH="/mingw64/include:/mingw64/x86_64-w64-mingw32/include"
+export LD_LIBRARY_PATH="/mingw64/lib:/mingw64/x86_64-w64-mingw32/lib"
